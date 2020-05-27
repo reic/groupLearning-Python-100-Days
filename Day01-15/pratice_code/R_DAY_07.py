17e9deed2d0fec961b45febdac7f78601b3ec218
reic <reic.wang@gmail.com>
2020-05-27 23:22:40 +0800

    r 07

--- Day01-15/pratice_code/R_DAY_07.py
+++ Day01-15/pratice_code/R_DAY_07.py
@@ -285,39 +285,89 @@ list 的元素添加和移除
 '''
 字典
 '''
-# 创建字典的字面量语法
-scores = {'骆昊': 95, '白元芳': 78, '狄仁杰': 82}
-print(scores)
-# 创建字典的构造器语法
-items1 = dict(one=1, two=2, three=3, four=4)
-# 通过zip函数将两个序列压成字典
-items2 = dict(zip(['a', 'b', 'c'], '123'))
-# 创建字典的推导式语法
-items3 = {num: num ** 2 for num in range(1, 10)}
-print(items1, items2, items3)
-# 通过键可以获取字典中对应的值
-print(scores['骆昊'])
-print(scores['狄仁杰'])
-# 对字典中所有键值对进行遍历
-for key in scores:
-    print(f'{key}: {scores[key]}')
-# 更新字典中的元素
-scores['白元芳'] = 65
-scores['诸葛王朗'] = 71
-scores.update(冷面=67, 方启鹤=85)
-print(scores)
-if '武则天' in scores:
-    print(scores['武则天'])
-print(scores.get('武则天'))
-# get方法也是通过键获取对应的值但是可以设置默认值
-print(scores.get('武则天', 60))
-# 删除字典中的元素
-print(scores.popitem())
-print(scores)
-print(scores.popitem())
-print(scores)
-print(scores.pop('骆昊'))
-print(scores)
-# 清空字典
-scores.clear()
-print(scores)
+# # 创建字典的字面量语法
+# scores = {'骆昊': 95, '白元芳': 78, '狄仁杰': 82}
+# print(scores)
+# # 创建字典的构造器语法
+# items1 = dict(one=1, two=2, three=3, four=4)
+# # 通过zip函数将两个序列压成字典
+# items2 = dict(zip(['a', 'b', 'c'], '123'))
+# # 创建字典的推导式语法
+# items3 = {num: num ** 2 for num in range(1, 10)}
+# print(items1, items2, items3)
+# # 通过键可以获取字典中对应的值
+# print(scores['骆昊'])
+# print(scores['狄仁杰'])
+# # 对字典中所有键值对进行遍历
+# for key in scores:
+#     print(f'{key}: {scores[key]}')
+# # 更新字典中的元素
+# scores['白元芳'] = 65
+# scores['诸葛王朗'] = 71
+# scores.update(冷面=67, 方启鹤=85)
+# print(scores)
+# if '武则天' in scores:
+#     print(scores['武则天'])
+# print(scores.get('武则天'))
+# # get方法也是通过键获取对应的值但是可以设置默认值
+# print(scores.get('武则天', 60))
+# # 删除字典中的元素
+# print(scores.popitem())
+# print(scores)
+# print(scores.popitem())
+# print(scores)
+# print(scores.pop('骆昊'))
+# print(scores)
+# # 清空字典
+# scores.clear()
+# print(scores)
+
+'''
+在營幕顯示跑馬燈文字
+顯示文字：群體的 Python 100 天學習記錄......
+'''
+
+def prac1():
+   import os
+   import time
+   content="群體的 Python 100 天學習記錄......" 
+   while True:
+       os.system('cls') # clear console
+       print(content.center(150))
+       content=content[1:]+content[0]
+       time.sleep(0.2) #sleep 的變數為秒數
+
+'''
+設計一個由數字和文字組成的 驗證碼，可以指定驗證碼長度
+'''
+def generate_code(code_len=4):
+    import random
+    # 產生 code_table
+    code_table="".join([chr(65+i) for i in range(26)]) #產生 A-Z
+    code_table+="".join([chr(97+i) for i in range(26)]) #產生 a-z
+    code_table+="".join([str(i) for i in range(10)])     #產生 0-9
+    last_pos=len(code_table)-1
+    code=''
+    for _ in range(code_len):
+        code+=code_table[random.randint(0,last_pos)]
+    return code
+def generate_code1(code_len=4):
+    import random
+    code=''
+    for _ in range(code_len):
+        type_char=random.randint(1,3) # 1 數字， 2 大寫, 3 小寫
+        if type_char==1:
+            code+=str(random.randint(0,9))
+        elif type_char==2:
+            code+=chr(64+random.randint(1,26)) #char(65) 為 A
+        else:
+            code+=chr(96+random.randint(1,26)) #char(97) 為 a
+    return code
+
+
+
+if __name__=="__main__":
+    # prac1()
+    print(generate_code())
+    print(generate_code1(5))
+
