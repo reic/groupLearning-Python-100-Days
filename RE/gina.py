e33620fe74046929fe8930ddd211d94b58caeb53
reic <reic.wang@gmail.com>
2020-07-17 09:16:47 +0800

    update_gina answer check

--- RE/gina.py
+++ RE/gina.py
@@ -16,14 +16,17 @@ def get_content(path):
     return data
 
 
-def get_unit_data(path, quiztype=1, numtest=10):
+# def get_unit_data(path, quiztype=1, numtest=10):
+def get_unit_data(path, quiztype=1):
+    ''' path 用 list 儲放，合併成 string'''
     text = get_content(''.join(path))
 
     maxnumber = len(text['english'])
     # for i in range(maxnumber):
     #     print(" %20s  %20s \t %s" %
     #           (text['english'][i], text['syllable'][i], text['mylanguage'][i]))
-    quiz = random.sample(range(0, maxnumber), numtest)
+    # quiz = random.sample(range(0, maxnumber), numtest)
+    quiz = list(range(0, maxnumber))
     if quiztype == 1:
         ques = ['mylanguage', 'english']
     elif quiztype == 2:
@@ -31,39 +34,60 @@ def get_unit_data(path, quiztype=1, numtest=10):
     else:
         ques = ['mylanguage', 'english']
     print("Quiz start:")
-    for i in quiz:
+    # for i in quiz:
+    while len(quiz) > 0:
+        i = random.choice(quiz)
         # if ans == text[ques[1]][i]:
         if quiztype != 3:
-            ans = input(" %s ： " % text[ques[0]][i]).lower().strip()
-            if ans in text[ques[1]][i]:
+            print("%s" % text[ques[0]][i])
+            ans = input("ans: ").lower().strip()
+            # if ans in text[ques[1]][i]:
+            if answer_check(ans, text[ques[1]][i], quiztype):
                 if quiztype != 1:
-                    print("\t 答對了。 %s" % text[ques[1]][i])
+                    print("%5s答對了。 %s" % ('', text[ques[1]][i]))
                 else:
-                    print("\t 答對了。")
+                    print("%5s答對了。" % (''))
+                    get_voice(path, i)
+                quiz.remove(i)
             else:
-                print("\t Error： Answer is: %s " % text[ques[1]][i])
+                print("%5sError： Answer is: %s " % ('', text[ques[1]][i]))
+                sleep(1.5)
         else:
             while 1:
                 get_voice(path, i)
                 ans = input("再聽一次，請輸入 y ：").lower()
                 if ans != "y":
-                    if ans in text[ques[1]][i]:
-                        print("\t 答對了， %s " % text[ques[0]][i])
+                    # if ans == text[ques[1]][i]:
+                    if answer_check(ans, text[ques[1]][i], quiztype):
+                        print("%5s答對了， %s " % ('', text[ques[0]][i]))
+                        quiz.remove(i)
                     else:
-                        print("\t 錯， %s %s" %
-                              (text[ques[1]][i], text[ques[0]][i]))
+                        print("%5sError: %s %s" %
+                              ('', text[ques[1]][i], text[ques[0]][i]))
+                        sleep(1.5)
                     break
         sleep(1.5)
         os.system('cls')
 
 
+def answer_check(ans, ques_ans, quiztype):
+    ''' 檢查不同 quiztype 答案是否正確 '''
+    if quiztype == 2:
+        # '''中文 unicode 範圍 '''
+        ch_ans = re.findall(u"[\u4e00-\u9fa5]+", ques_ans)
+        return ans in ch_ans
+    else:
+        return ans == ques_ans
+
+
 def get_voice(path, filenum):
     path_dir = ''.join(path)
     pre = path[1].split('-')
-    pre_text = '2ku%d' % int(pre[1])
-    if len(pre) > 2:
-        pre_text += 'u%d' % int(pre[2])
-    pre_text += 'w'
+    # pre_text = '2ku%d' % int(pre[1])
+    # if len(pre) > 2:
+    #     pre_text += 'u%d' % int(pre[2])
+    # pre_text += 'w'
+    pre_text = '2ku'+'u'.join(pre[1:])+'w'
     # voicelist = random.sample(range(36), 4)
     # for i in voicelist:
     filename = '%s%s%02d.mp3' % (path_dir, pre_text, filenum+1)
