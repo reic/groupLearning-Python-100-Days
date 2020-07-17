dcb14468edc66aeb72679bcbff55a78898e5fbec
reic <reic.wang@gmail.com>
2020-07-17 17:07:52 +0800

    doc reading_to count unexception rate

--- RE/doc.py
+++ RE/doc.py
@@ -27,7 +27,7 @@ def unknownwords_to_file(filename, data):
 def main():
     # 從 word 檔讀取文字， 不認識的字  double underline (ctrl - shift - d) 的字
     wordall, unknowns = get_doc_words('test.docx')
-    print(wordall)
+    # print(wordall)
     stemlist = word_count(wordall)
     unknown_stemlist = word_count(unknowns)
     counter = 0
@@ -37,8 +37,10 @@ def main():
         unknown_stemlist[item].counter = stemlist[item].counter
     accident_rate = counter/len(wordall)*100
     # print(counter, len(wordall))
-    print(counter)
+    # print(counter)
     print({item.word: item.counter for item in unknown_stemlist.values()})
+    print("總共單字數 %2d" % len(stemlist))
+    print("不熟悉的單字 %2d" % len(unknown_stemlist))
     print("意外率 %.2f %%" % accident_rate)
 
     # 將新的單字寫入檔案
