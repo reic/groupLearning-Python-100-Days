222c002ead83789b837a045dc7773857e8734367
reic <reic.wang@gmail.com>
2020-09-10 14:09:48 +0800

    update_code

--- RE/excels_to_one.py
+++ RE/excels_to_one.py
@@ -16,15 +16,17 @@ def main():
 
     impact_dict = {}
     for filename in filelists[:1]:
-    ''' 讀取 A 欄，建立國家名稱'''
-    data_df = pd.read_excel(filepath+"/"+filename, usecols=[0], header=None)
-    impact_dict.update({"國家": data_df[0][8:].values})
+        ''' 讀取 A 欄，建立國家名稱 '''
+        data_df = pd.read_excel(filepath+"/"+filename,
+                                usecols=[0], header=None)
+        impact_dict.update({"國家": data_df[0][8:].values})
 
     for filename in filelists:
-    ''' 將不同的 xlsx 檔，整理成一個 excel '''
-    data_df = pd.read_excel(filepath+"/"+filename, usecols=[1], header=None)
-    dic_index = data_df[1][1]
-    impact_dict.update({dic_index: data_df[1][8:].values})
+        ''' 將不同的 xlsx 檔，整理成一個 excel '''
+        data_df = pd.read_excel(filepath+"/"+filename,
+                                usecols=[1], header=None)
+        dic_index = data_df[1][1]
+        impact_dict.update({dic_index: data_df[1][8:].values})
 
     toexcel = pd.DataFrame(impact_dict)
     toexcel.to_excel("output.xlsx")
