88253c8d3dfb923429419fa11c169a599d58636d
unknown <hcwang@stpi.org.tw>
2020-09-21 16:35:41 +0800

    submit

--- Day21-30/R_Day_21.py
+++ Day21-30/R_Day_21.py
@@ -13,14 +13,36 @@ def make_sample_file(path, num=10):
             f.write("This is the %02d file" % filename)
 
 
+def create_batch(base_dir, target_dir, batchfilename):
+    if os.path.isfile(batchfilename):
+        os.remove(batchfilename)
+    with open(batchfilename, 'a+', encoding='utf=8') as f:
+        f.write('if not exist output\\ mkdir output\n')
+        for itm in os.walk(base_dir):
+            midtext = ''
+            if len(itm[2]) == 0:
+                continue
+            if len(itm[1]) != 0:
+                midtext = "%s_" % itm[1][0]
+            for filename in itm[2]:
+                f.write('move "%s\\%s" "%s\\%s%s"\n' %
+                        (itm[0], filename, target_dir, midtext, filename))
+    pass
+
+
 def main():
-    sample_path = "sample"
+    sample_path = "sample"  # 檔案的位置
+    target_path = 'output'  # 將檔案移動到的目標區域
+    batchfilename = 'output.bat'  # batchfilename 在 cmd 下執行的
     if not os.path.exists(sample_path):
         os.mkdir(sample_path)
     make_sample_file(sample_path)
+    # 建立 batch file in UTF-8
+    create_batch(sample_path, target_path, batchfilename)
+    # 若是檔案名稱有中文，建議再用 notepad 將 batch file 轉成 ANSI 的編碼
 
 
 if __name__ == '__main__':
-    # main()
+    main()
     # sh.rmtree("sample")
     pass
