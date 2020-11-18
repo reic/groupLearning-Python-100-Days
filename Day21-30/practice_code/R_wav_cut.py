2389f7fd6ccc86318b20759c58db94ba75625e97
unknown <hcwang@stpi.org.tw>
2020-11-18 09:40:55 +0800

    vocie to text _ pratice

---
+++ Day21-30/practice_code/R_wav_cut.py
@@ -0,0 +1,96 @@
+
+import speech_recognition as sr
+import os
+import wave
+import numpy as np
+import pylab as plt
+
+
+def CutFile(path, FileName, target_path):
+
+    print("CutFile File Name is ", FileName)
+    f = wave.open(path+"\\" + FileName, "rb")
+    params = f.getparams()
+    print(params)
+    nchannels, sampwidth, framerate, nframes = params[:4]
+    CutFrameNum = framerate * CutTimeDef
+    # 讀取格式資訊
+    # 一次性返回所有的WAV檔案的格式資訊，它返回的是一個組元(tuple)：聲道數, 量化位數（byte    單位）, 採
+    # 樣頻率, 取樣點數, 壓縮型別, 壓縮型別的描述。wave模組只支援非壓縮的資料，因此可以忽略最後兩個資訊
+
+    print("CutFrameNum=%d" % (CutFrameNum))
+    print("nchannels=%d" % (nchannels))
+    print("sampwidth=%d" % (sampwidth))
+    print("framerate=%d" % (framerate))
+    print("nframes=%d" % (nframes))
+    str_data = f.readframes(nframes)
+    f.close()  # 將波形資料轉換成陣列
+    # Cutnum =nframes/framerate/CutTimeDef
+    # 需要根據聲道數和量化單位，將讀取的二進位制資料轉換為一個可以計算的陣列
+    wave_data = np.fromstring(str_data, dtype=np.short)
+    wave_data.shape = -1, 2
+    wave_data = wave_data.T
+    temp_data = wave_data.T
+    # StepNum = int(nframes/200)
+    StepNum = CutFrameNum
+    StepTotalNum = 0
+    haha = 0
+    while StepTotalNum < nframes:
+        # for j in range(int(Cutnum)):
+        print("Stemp=%d" % (haha))
+        SaveFile = "%s-%03d.wav" % (FileName[:-4], (haha+1))
+        print(FileName)
+        temp_dataTemp = temp_data[StepNum * (haha):StepNum * (haha + 1)]
+        haha = haha + 1
+        StepTotalNum = haha * StepNum
+        temp_dataTemp.shape = 1, -1
+        temp_dataTemp = temp_dataTemp.astype(np.short)  # 開啟WAV文件
+        f = wave.open(target_path + SaveFile, "wb")
+        # 配置聲道數、量化位數和取樣頻率
+        f.setnchannels(nchannels)
+        f.setsampwidth(sampwidth)
+        f.setframerate(framerate)
+        # 將wav_data轉換為二進位制資料寫入檔案
+        f.writeframes(temp_dataTemp.tostring())
+        f.close()
+
+
+def VoiceToText(path, files, target_path):
+    for file in files:
+        with open("%s\\%s.txt" % (target_path, file[:-4]), "w", encoding="utf-8") as f:
+            f.write("%s:\n" % file)
+            r = sr.Recognizer()  # 預設辨識英文
+            with sr.WavFile(path+"\\"+file) as source:  # 讀取wav檔
+                audio = r.record(source)
+            try:
+                text = r.recognize_google(audio, language="zh-TW")
+                print(file)
+                if len(text) == 0:
+                    print("===無資料==")
+                    continue
+
+                print(text)
+                f.write("%s \n\n" % text)
+            except sr.RequestError as e:
+                print("無法翻譯{0}".format(e))
+                # 兩個 except 是當語音辨識不出來的時候 防呆用的
+                # 使用Google的服務
+            except LookupError:
+                print("Could not understand audio")
+
+
+if __name__ == "__main__":
+    # # Cut Wave Setting
+    CutTimeDef = 30  # 以1s截斷檔案
+    # # CutFrameNum =0
+    # FileName = "201116_2.wav"
+    # path = "g:\\"
+    # target_path = "g:\\wav2\\"
+
+    # CutFile(path, FileName, target_path)
+    path = "g:\\wav2\\"
+    target_path = "g:\\txt2\\"
+    files = os.listdir(path)
+    files = files[136:]
+
+    VoiceToText(path, files, target_path)
